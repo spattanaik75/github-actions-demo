3aeb82870b80fa8818df35c7a3e0956a4699be8d
Soumya Pattanaik <soumya.pattanaik@securitease.com>
2023-08-23 14:18:23 +0200

    first commit

---
+++ src/test/java/corp/acme/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package corp.acme.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
